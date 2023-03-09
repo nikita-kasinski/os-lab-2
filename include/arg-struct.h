e5c0fddcb11cdb225d155c7a6c28734149f33f0f
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-03-09 15:14:09 +0300

    remove const qualifiers

--- include/arg-struct.h
+++ include/arg-struct.h
@@ -6,9 +6,9 @@
 class arg_struct
 {
 public:
-    const int* array;
-    const size_t size;
-    int* return_values = nullptr;
+    int* array;
+    size_t size;
+    int** return_values = nullptr;
     int return_size;
     ~arg_struct()
     {
