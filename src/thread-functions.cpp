f3dc061b8842500b2a235a8cb4d089ba9ea6d743
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-03-09 15:29:05 +0300

    implement average function

--- src/thread-functions.cpp
+++ src/thread-functions.cpp
@@ -33,6 +33,32 @@ DWORD WINAPI min_max(LPVOID args)
                 Sleep(7);
             }
         }
+        std::cout << "Minimum : " << **min << "\n";
+        std::cout << "Maximum : " << **max << "\n";
     }
+}
 
+DWORD WINAPI average(LPVOID args)
+{
+    average_args *args_pointer = (average_args*)args;
+    if (args_pointer -> size == 0)
+    {
+        std::cerr << "Array is empty. Exit thread\n";
+        ExitThread(1);
+    }
+    else
+    {
+        int *array = args_pointer->array;
+        const size_t size = args_pointer->size;
+        int *average = &args_pointer->average;
+        int sum = 0;
+        for (size_t i = 0; i < size; ++i)
+        {
+            sum += array[i];
+        }
+        double real_average = sum / (double)size;
+        *average = (real_average + 0.5F);
+        std::cout << "Average : " << real_average << "\n";
+        std::cout << "Since source array is of integer type it will be rounded to " << *average << "\n";
+    }
 }
