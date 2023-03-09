d733f3d589eceeb79305205c32a79dcd8068638c
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-03-09 15:47:38 +0300

    add threads' output handling

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main()
 	{
 		int *array = new int[size];
 		std::cout << "Enter " << size << " integers:\n";
-		for (int i = 0; i < size; ++i)
+		for (size_t i = 0; i < size; ++i)
 		{
 			std::cin >> array[i];
 		}
@@ -30,13 +30,23 @@ int main()
 		a_args.size = size;
 
 		DWORD minmaxThreadId;
-		HANDLE minmaxThread = CreateThread(NULL,0, min_max, (void*) &m_args, 0, &minmaxThreadId);
+		HANDLE minmaxThread = CreateThread(NULL, 0, min_max, (void*) &m_args, 0, &minmaxThreadId);
 		WaitForSingleObject(minmaxThread, INFINITE);
+		CloseHandle(minmaxThread);
 
 		DWORD averageThreadId;
-		HANDLE averageThread = CreateThread(NULL,0, average, (void*) &a_args, 0, &averageThreadId);
+		HANDLE averageThread = CreateThread(NULL, 0, average, (void*) &a_args, 0, &averageThreadId);
 		WaitForSingleObject(averageThread, INFINITE);
+		CloseHandle(averageThread);
 
+		*m_args.min = a_args.average;
+		*m_args.max = a_args.average;
+		std::cout << "Modified array:\n";
+		for (size_t i = 0; i < size; ++i)
+		{
+			std::cout << array[i] << ' ';
+		}
+		std::cout << "\n";
 		delete[] array;
 	}
 	return 0;
