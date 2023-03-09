2164c4de2a13228ea9354d2b5e6346f7fa02f307
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-03-09 15:41:52 +0300

    implement thread creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 #include <windows.h>
-
-using namespace std;
+#include "thread-functions.h"
+#include "arg-struct.h"
 
 int main()
 {
-	cout << "Hello world\n";
+	int size = 0;
+	std::cout << "Enter array size:\n";
+	std::cin >> size;
+	if (size < 0)
+	{
+		std::cerr << "Array size can't be negative\n";
+		return 1;
+	}
+	else
+	{
+		int *array = new int[size];
+		std::cout << "Enter " << size << " integers:\n";
+		for (int i = 0; i < size; ++i)
+		{
+			std::cin >> array[i];
+		}
+		minmax_args m_args;
+		m_args.array = array;
+		m_args.size = size;
+
+		average_args a_args;
+		a_args.array = array;
+		a_args.size = size;
+
+		DWORD minmaxThreadId;
+		HANDLE minmaxThread = CreateThread(NULL,0, min_max, (void*) &m_args, 0, &minmaxThreadId);
+		WaitForSingleObject(minmaxThread, INFINITE);
+
+		DWORD averageThreadId;
+		HANDLE averageThread = CreateThread(NULL,0, average, (void*) &a_args, 0, &averageThreadId);
+		WaitForSingleObject(averageThread, INFINITE);
+
+		delete[] array;
+	}
 	return 0;
 }
-
