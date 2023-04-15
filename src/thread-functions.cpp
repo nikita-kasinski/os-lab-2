3988b97933604beedee5319271446815e23b69f0
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 17:34:41 +0300

    use utility methods in minmaxthread, add critical sections

--- src/thread-functions.cpp
+++ src/thread-functions.cpp
@@ -1,54 +1,45 @@
-//author: Nikita Kasinski
+// author: Nikita Kasinski
 #include <windows.h>
-#include <vector>
 #include <iostream>
 #include "arg-struct.h"
+#include "utility.h"
 
 DWORD WINAPI min_max(LPVOID args)
 {
-    minmax_args *args_pointer = (minmax_args*)args;
-    if (args_pointer -> size == 0)
+    minmax_args *args_pointer = (minmax_args *)args;
+    CRITICAL_SECTION *iocs = args_pointer->iocs;
+
+    args_pointer->indexMin = Utility::getMinimumIndexWithSleep(args_pointer->array, args_pointer->size);
+    args_pointer->indexMax = Utility::getMaximumIndexWithSleep(args_pointer->array, args_pointer->size);
+
+    if (args_pointer->indexMin == -1 || args_pointer->indexMax == -1)
     {
+        EnterCriticalSection(iocs);
         std::cerr << "Array is empty. Exit thread\n";
+        LeaveCriticalSection(iocs);
         ExitThread(1);
     }
-    else
-    {
-        int *array = args_pointer->array;
-        const size_t size = args_pointer->size;
-        int **min = &args_pointer->min;
-        int **max = &args_pointer->max;
-        *min = &array[0];
-        *max = &array[0];
-        for (size_t i = 0; i < size; ++i)
-        {
-            if (**min > array[i])
-            {
-                *min = &array[i];
-                Sleep(7);
-            }
-            if (**max < array[i])
-            {
-                *max = &array[i];
-                Sleep(7);
-            }
-        }
-        std::cout << "Minimum : " << **min << "\n";
-        std::cout << "Maximum : " << **max << "\n";
-    }
+
+    int min = args_pointer->array[args_pointer->indexMin];
+    int max = args_pointer->array[args_pointer->indexMax];
+
+    EnterCriticalSection(iocs);
+    std::cout << "Minimum : " << min << "\n";
+    std::cout << "Maximum : " << max << "\n";
+    LeaveCriticalSection(iocs);
 }
 
 DWORD WINAPI average(LPVOID args)
 {
-    average_args *args_pointer = (average_args*)args;
-    if (args_pointer -> size == 0)
+    average_args *args_pointer = (average_args *)args;
+    if (args_pointer->size == 0)
     {
         std::cerr << "Array is empty. Exit thread\n";
         ExitThread(1);
     }
     else
     {
-        int *array = args_pointer->array;
+        const int *array = args_pointer->array;
         const size_t size = args_pointer->size;
         int *average = &args_pointer->average;
         int sum = 0;
