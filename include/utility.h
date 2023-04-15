5e1ce126aace3f33d624d6165898b39cf4d05ac2
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 16:56:53 +0300

    comment min max and avg methods

--- include/utility.h
+++ include/utility.h
@@ -6,7 +6,13 @@ class Utility
 private:
 
 public:
+
+    // returns -1 if size is zero
     static size_t getMinimumIndexWithSleep(const int* array, size_t size);
+
+    // returns -1 if size is zero
     static size_t getMaximumIndexWithSleep(const int* array, size_t size);
+
+    // returns -1 if size is zero
     static size_t getAverageWithSleep(const int* array, size_t size);
-}
+};
