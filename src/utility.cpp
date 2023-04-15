c8870541c094af307e84ed3f5970e64d152604fb
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 17:18:27 +0300

    implement getAverage method

--- src/utility.cpp
+++ src/utility.cpp
@@ -38,3 +38,24 @@ size_t Utility::getMaximumIndexWithSleep(const int* array, size_t size)
 
     return maxIndex;
 }
+
+double Utility::getAverageWithSleep(const int* array, size_t size, bool& ok)
+{
+    if (size == 0)
+    {
+        ok = false;
+        return 0;
+    }
+
+    ok = true;
+    int sum = 0;
+    for (size_t i = 0; i < size; ++i)
+    {
+        sum += array[i];
+        Sleep(Utility::avgSleep);
+    }
+
+    double ans = sum / static_cast<double>(size);
+
+    return ans;
+}
