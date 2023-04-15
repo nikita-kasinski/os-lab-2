b32e4f91a0f263c1193eb7097c7f4467c9324519
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 17:18:14 +0300

    change getAverage return type to double

--- include/utility.h
+++ include/utility.h
@@ -16,6 +16,5 @@ public:
     // returns -1 if size is zero
     static size_t getMaximumIndexWithSleep(const int* array, size_t size);
 
-    // returns -1 if size is zero
-    static size_t getAverageWithSleep(const int* array, size_t size);
+    static double getAverageWithSleep(const int* array, size_t size, bool& ok);
 };
